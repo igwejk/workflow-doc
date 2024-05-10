4a0253b7ddac1597f75629f0f90d2220ed6df0f7
Igwe Kalu <igwejk@github.com>
2024-05-10 19:36:38 +0200

    WIP

--- workflowdoc.py
+++ workflowdoc.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-from io import TextIOWrapper
+"""
+Generate markdown documentation for a given GitHub Actions workflow. 
+
+It includes the following functions:
+
+- `workflowdoc()`: Command name.
+- `read_workflow_description(workflow_file_content: str) -> str`:
+  Reads the description of the workflow from the workflow file.
+- `generate_markdown(workflow_path: str) -> Generator[str, None, None]`:
+  Generates a markdown documentation for the analysed workflow.
+- `generate(workflow_path: str) -> None`:
+  Generates a markdown documentation for the workflow file at the specified path.
+"""
+
 from os import path
-import pathlib
-import yaml
+from pathlib import Path
+from typing import Generator
+
 import typer
+import yaml
 
 
 def workflowdoc() -> None:
@@ -17,13 +32,12 @@ def workflowdoc() -> None:
 app = typer.Typer(callback=workflowdoc, name="workflowdoc")
 
 
-def read_workflow_description(workflow_file: TextIOWrapper) -> str:
+def read_workflow_description(workflow_file_content: str) -> str:
     """
     Read the description of the workflow from the workflow file.
     """
 
-    workflow_file.seek(0)
-    workflow_lines = workflow_file.readlines()
+    workflow_lines = workflow_file_content.split("\n")
 
     workflow_description = None
     for line in workflow_lines:
@@ -42,85 +56,143 @@ def read_workflow_description(workflow_file: TextIOWrapper) -> str:
     return workflow_description
 
 
-def analysed_workflow_inputs(workflow_inputs: dict) -> dict:
+def render_workflow_dispatch_input_type_md_table_cell(input_details: dict) -> str:
     """
-    Analyse the inputs of the workflow and return the analysis results.
-    """
-    return {**workflow_inputs}
-
-
-def analysed_workflow_outputs(workflow_outputs: dict) -> dict:
-    """
-    Analyse the outputs of the workflow and return the analysis results.
-    """
-    return {**workflow_outputs}
-
-
-def analysed_workflow_secrets(workflow_secrets: dict) -> dict:
-    """
-    Analyse the outputs of the workflow and return the analysis results.
-    """
-    return {**workflow_secrets}
-
-
-def analyse_workflow(workflow_path: str) -> dict:
-    """
-    Analyse the workflow file at the specified path and return the analysis results.
+    Render the markdown for the input type of the workflow dispatch input.
     """
 
-    with open(workflow_path, mode="r", encoding="utf-8") as workflow_file:
-        workflow = yaml.safe_load(stream=workflow_file)
-        workflow_description = read_workflow_description(workflow_file=workflow_file)
+    input_type = input_details.get("type")
 
-    analysed_workflow = {"workflow_description": workflow_description}
+    if input_type == "choice":
+        input_options = input_details.get("options", [])
+        input_options_li = "".join([f"<li>{option}</li>" for option in input_options])
+        input_options_ul = f"<ul>{input_options_li}</ul>"
 
-    if "workflow_call" in workflow["on"]:
-        analysed_workflow.update(
-            {
-                "is_reusable": True,
-                "inputs": analysed_workflow_inputs(
-                    workflow_inputs=workflow["on"]["workflow_call"].get("inputs", None)
-                ),
-                "outputs": analysed_workflow_outputs(
-                    workflow_outputs=workflow["on"]["workflow_call"].get(
-                        "outputs", None
-                    )
-                ),
-                "secrets": analysed_workflow_secrets(
-                    workflow_secrets=workflow["on"]["workflow_call"].get(
-                        "secrets", None
-                    )
-                ),
-            }
-        )
+        return f"`{input_type}`:<br />{input_options_ul}"
 
-    if "workflow_dispatch" in workflow["on"]:
-        analysed_workflow.update(
-            {
-                "is_triggered_manually": True,
-                "inputs": analysed_workflow_inputs(
-                    workflow_inputs=workflow["on"]["workflow_dispatch"].get(
-                        "inputs", None
-                    )
-                ),
-            }
-        )
-
-    if "workflow_run" in workflow["on"]:
-        analysed_workflow.update(
-            {
-                "is_triggered_by_execution_of_another_workflow": True,
-                "triggered_by_execution_of_workflow": workflow["on"]["workflow_run"],
-            }
-        )
+    return f"`{input_type}`"
 
 
-def generate_markdown(analysed_workflow: dict) -> str:
+def generate_markdown(workflow_path: str) -> Generator[str, None, None]:
     """
     Generate a markdown documentation for the analysed workflow.
     """
 
-    return ""
+    workflow_file_content = Path(workflow_path).read_text(encoding="utf-8")
+    workflow = yaml.safe_load(workflow_file_content)
+
+    yield f"# {workflow['name']}"
+    yield "\n"
+    yield read_workflow_description(workflow_file_content=workflow_file_content)
+    yield "\n"
+
+    yield "## Triggers"
+    yield "\n"
+
+    if "workflow_call" in workflow["on"]:
+        yield "### `workflow_call`"
+        yield "\n"
+        yield "This workflow is reusable."
+        yield "\n"
+
+        if workflow["on"]["workflow_call"].get("inputs", None):
+            yield "#### Inputs"
+            yield "| Name | Description | Default | Required | Type |"
+            yield "| :--- | :---------- | :------ | :------: | :--- |"
+
+            for input_name, input_details in workflow["on"]["workflow_call"][
+                "inputs"
+            ].items():
+                yield (
+                    f"| {input_name} "
+                    f"| {input_details.get('description', '')} "
+                    f"| {input_details.get('default', '')} "
+                    f"| {'*' if input_details.get('required', False) else ''} "
+                    f"| `{input_details.get('type')}` |"
+                )
+
+        if workflow["on"]["workflow_call"].get("secrets", None):
+            yield "#### Secrets"
+            yield "| Name | Description | Required |"
+            yield "| :--- | :---------- | :------: |"
+
+            for secret_name, secret_details in workflow["on"]["workflow_call"][
+                "secrets"
+            ].items():
+                yield (
+                    f"| {secret_name} "
+                    f"| {secret_details.get('description', '')} "
+                    f"| {secret_details.get('required', False)} |"
+                )
+
+        if workflow["on"]["workflow_call"].get("outputs", None):
+            yield "#### Outputs"
+            yield "| Name | Description |"
+            yield "| :--- | :---------- |"
+
+            for output_name, output_details in workflow["on"]["workflow_call"][
+                "outputs"
+            ].items():
+                yield (
+                    f"| {output_name} " f"| {output_details.get('description', '')} |"
+                )
+
+    if "workflow_dispatch" in workflow["on"]:
+        yield "### `workflow_dispatch`"
+        yield "\n"
+        yield "This workflow can be manually triggered."
+        yield "\n"
+
+        if workflow["on"]["workflow_dispatch"].get("inputs", None):
+            yield "#### Inputs"
+            yield "| Name | Description | Default | Required | Type |"
+            yield "| :--- | :---------- | :------ | :------: | :--- |"
+
+            for input_name, input_details in workflow["on"]["workflow_dispatch"][
+                "inputs"
+            ].items():
+                type_table_value = render_workflow_dispatch_input_type_md_table_cell(
+                    input_details=input_details
+                )
+                yield (
+                    f"| {input_name} "
+                    f"| {input_details.get('description', '')} "
+                    f"| {input_details.get('default', '')} "
+                    f"| {'*' if input_details.get('required', False) else ''} "
+                    f"| {type_table_value} |"
+                )
+
+    if "workflow_run" in workflow["on"]:
+        yield "### `workflow_run`"
+        yield "\n"
+        yield "This workflow is triggered by the execution of other workflows."
+        yield "\n"
+
+        yield "#### Triggering workflows:"
+        yield "\n"
+        for workflow_name in workflow["on"]["workflow_run"]["workflows"]:
+            yield f"- {workflow_name}"
+        yield "\n"
+
+        branches = workflow["on"]["workflow_run"].get("branches", None) or workflow[
+            "on"
+        ]["workflow_run"].get("branches-ignore", None)
+        if branches:
+            yield (
+                "#### Workflow branches:"
+                if workflow["on"]["workflow_run"].get("branches", None)
+                else "#### `Skipped` workflow branches:"
+            )
+            yield "\n"
+            for branch in branches:
+                yield f"- {branch}"
+            yield "\n"
+
+        yield "#### Workflow activity types:"
+        activity_types = workflow["on"]["workflow_run"].get("types")
+        for activity_type in activity_types:
+            yield f"- {activity_type}"
+        yield "\n"
 
 
 @app.command(name="generate")
@@ -133,18 +205,19 @@ def generate(
     Generate a markdown documentation for the workflow file at the specified path.
     """
 
-    print(f"Analysing workflow {workflow_path} ...")
-    analysed_workflow = analyse_workflow(workflow_path=workflow_path)
-
     print(f"Generating markdown documentation for workflow {workflow_path} ...")
-    doc_markdown = generate_markdown(analysed_workflow=analysed_workflow)
+    generated_output_path = path.join(
+        path.dirname(workflow_path), f"{path.basename(workflow_path)}.md"
+    )
 
     with open(
-        path.join(path.dirname(workflow_path), path.basename(workflow_path)),
-        mode="w",
-        encoding="utf-8",
+        mode="wt", file=generated_output_path, encoding="utf-8"
     ) as generated_doc_file:
-        generated_doc_file.write(doc_markdown)
+
+        for line in generate_markdown(workflow_path=workflow_path):
+            print(line, file=generated_doc_file)
+
+        generated_doc_file.flush()
 
 
 if __name__ == "__main__":
